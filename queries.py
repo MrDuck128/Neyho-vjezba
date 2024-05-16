341d530141b9c0fc00dacaa5284fda06018f016c
Dominik Kinkela <50632865+MrDuck128@users.noreply.github.com>
2024-05-16 18:11:37 +0200

    fix

--- queries.py
+++ queries.py
@@ -4,15 +4,6 @@ import json
 with open('data.txt') as f:
   fullData = json.load(f)
 
-newData = []
-for entry in fullData:
-  e = {
-    'vjetar_brzina': float(entry['vjetar_brzina']),
-    'temperatura_zraka': float(entry['temperatura_zraka']),
-    'tlak_zraka': float(entry['tlak_zraka']),
-  }
-  newData.append(e)
-
 query = """
 mutation ($dhmz: [DhmzMjerenjaInput]) {
   stackDhmzMjerenjaList (dhmz_mjerenja: $dhmz) {
