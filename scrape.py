4a55a69528a7c4e015fa0bb7fd2241fe6469f988
Dominik Kinkela <50632865+MrDuck128@users.noreply.github.com>
2024-05-21 00:29:13 +0200

    Scrape fix

--- scrape.py
+++ scrape.py
@@ -36,7 +36,6 @@ def getData():
             EC.presence_of_element_located((By.ID, 'table-aktualni-podaci'))
         )
 
-
         dataElements = driver.find_elements(By.XPATH, '//div[@class="fd-c-table--responsive fd-u-display--none fd-u-md-display--block"]/table/tbody/tr/td')
 
         data = [re.sub(' A$|\*$', '', dat.text) for dat in dataElements]
@@ -47,8 +46,7 @@ def getData():
                 dataProcessed.append(data[i*8:(i+1)*8])
 
         dateElement = driver.find_element(By.XPATH, '//div[@class="glavni__content"]/*/h4').text.split(' ')
-        date = dateElement[3]
-        time = dateElement[5]
+        date, time = dateElement[3], dateElement[5]
 
         for entry in dataProcessed:
             e = {
@@ -65,6 +63,21 @@ def getData():
             }
 
             dataList.append(e)
+
+        if dfPrint:
+            columnTitlesElements = driver.find_elements(By.XPATH, '//div[@class="fd-c-table--responsive fd-u-display--none fd-u-md-display--block"]/table/thead/tr/th')
+            columnTitles = [col.text.replace('\n', ' ') for col in columnTitlesElements]
+            numberOfColumns = len(columnTitles)
+
+            columnTitles.append('Datum i vrijeme')
+
+            for i in range(len(dataProcessed)):
+                dataProcessed[i].append(f'{date} u {time}h')
+
+            df = pd.DataFrame(dataProcessed, columns=columnTitles)
+            print(f'Vrijeme u {hour} sati:')
+            print(df.head(10))
+            # df.to_csv('dhmz_podaci.csv')
     
     with open('data.txt', 'w', encoding='utf-8') as f:
         json.dump(dataList, f)
@@ -73,67 +86,5 @@ def getData():
 
 
 if __name__ == '__main__':
+    dfPrint = 1
     getData()
-    service = Service(executable_path='chromedriver.exe')
-    driver = webdriver.Chrome(service=service)
-
-    driver.get('https://meteo.hr/podaci.php?section=podaci_vrijeme&param=hrvatska1_n&sat')
-
-    # https://meteo.hr/podaci.php?section=podaci_vrijeme&param=hrvatska1_n&sat=01
-
-    # hours = ['00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23']
-
-    hours = ['00']
-
-    dataList = []
-
-    sleep(5)
-
-    for hour in hours:
-
-        WebDriverWait(driver, 3).until(
-            EC.presence_of_element_located((By.ID, 'table-aktualni-podaci'))
-        )
-
-        hourElement = driver.find_element(By.XPATH, f'//ul[@class="hours-browser-v2__hours"]/li/*[contains(text(), "{hour}")]')
-        hourElement.click()
-
-        sleep(5)
-
-        WebDriverWait(driver, 3).until(
-            EC.presence_of_element_located((By.ID, 'table-aktualni-podaci'))
-        )
-
-        columnTitlesElements = driver.find_elements(By.XPATH, '//div[@class="fd-c-table--responsive fd-u-display--none fd-u-md-display--block"]/table/thead/tr/th')
-
-        columnTitles = [col.text.replace('\n', ' ') for col in columnTitlesElements]
-        numberOfColumns = len(columnTitles)
-
-        dataElements = driver.find_elements(By.XPATH, '//div[@class="fd-c-table--responsive fd-u-display--none fd-u-md-display--block"]/table/tbody/tr/td')
-
-        data = [re.sub(' A$|\*$', '', dat.text) for dat in dataElements]
-
-        dataProcessed = []
-        if len(data) % numberOfColumns == 0:
-            for i in range(len(data) // numberOfColumns):
-                dataProcessed.append(data[i*numberOfColumns:(i+1)*numberOfColumns])
-
-        columnTitles.append('Datum i vrijeme')
-        dateElement = driver.find_element(By.XPATH, '//div[@class="glavni__content"]/*/h4').text.split(' ')
-        date = dateElement[3]
-        time = dateElement[5]
-
-        for i in range(len(dataProcessed)):
-            dataProcessed[i].append((date, time))
-
-        df = pd.DataFrame(dataProcessed, columns=columnTitles)
-        print(f'Vrijeme u {hour} sati:')
-        print(df.head(10))
-        # dataFrameList.append(df)
-
-        # df.to_csv('dhmz_podaci.csv')
-
-    # with open('podaci.pkl', 'w') as f:
-    #     pickle.dump(dataFrameList, f)
-
-    driver.quit()
